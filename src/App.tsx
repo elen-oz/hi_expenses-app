d6be45cda1051a61d7b99247ad02ef36788ec5a1
elen-oz <ozerova.lenka@gmail.com>
2023-12-21 17:37:50 +0100

    feat: add close button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { useState } from 'react';
+import Alert from './components/Alert';
 import Button from './components/Button';
 
 const App = () => {
+  const [alertVisible, setAlertVisible] = useState(false);
+
   return (
     <>
-      <Button color='primary' onClick={() => console.log('Clicked')}>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisible(false)}>My Alert</Alert>
+      )}
+      <Button color='primary' onClick={() => setAlertVisible(true)}>
         My Button
       </Button>
     </>
