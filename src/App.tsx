d1f73e3f5ccd4f666264dd527f044e241ef9ed82
elen-oz <ozerova.lenka@gmail.com>
2023-12-22 18:35:42 +0100

    add likes

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,6 @@
-import { useState } from 'react';
-import Alert from './components/Alert';
-import Button from './components/Button/Button';
-import ListGroup from './components/ListGroup/ListGroup';
 import Like from './components/Like';
 
 const App = () => {
-  // const [alertVisible, setAlertVisible] = useState(false);
-  // const items = ['Stockholm', 'Madrid', 'Moscow', 'Istanbul'];
-
-  // return (
-  //   <>
-  //     <ListGroup
-  //       heading='Cities'
-  //       items={items}
-  //       onSelectItem={() => console.log('Clicked')}
-  //       // 45 min
-  //     />
-  //     {alertVisible && (
-  //       <Alert onClose={() => setAlertVisible(false)}>My Alert</Alert>
-  //     )}
-  //     <Button color='primary' onClick={() => setAlertVisible(true)}>
-  //       My Button
-  //     </Button>
-  //   </>
-  // );
   return <Like onClick={() => console.log('clicked')} />;
 };
 
--- src/components/Like.tsx
+++ src/components/Like.tsx
@@ -1,10 +1,20 @@
-import { CiHeart } from 'react-icons/ci';
+import { useState } from 'react';
+import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
 
 interface Props {
   onClick: () => void;
 }
 
 const Like = ({ onClick }: Props) => {
-  return <CiHeart onClick={onClick} />;
+  const [status, setStatus] = useState(false);
+
+  const toggle = () => {
+    setStatus(!status);
+    onClick();
+  };
+
+  if (status) return <AiFillHeart color='#ff6b81' size={20} onClick={toggle} />;
+
+  return <AiOutlineHeart size={20} onClick={toggle} />;
 };
 export default Like;
