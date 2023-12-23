534fb19e7f616f1f83eb9d1d3d0fa0ee633e0540
elen-oz <ozerova.lenka@gmail.com>
2023-12-23 20:11:26 +0100

    start form

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
 import { useState } from 'react';
 import Like from './components/Like';
 import ExpandableText from './components/ExpandableText';
+import Form from './components/Form';
 
 const App = () => {
-  const handleClick = () => {};
-
-  return (
-    // <Like onClick={handleClick} />
-    <ExpandableText>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam accusantium
-      assumenda, nobis quia nemo, veniam itaque voluptates at cumque labore
-      temporibus! Quo distinctio perferendis ipsa quasi, nisi laboriosam quis
-      quidem necessitatibus esse doloremque cumque laborum suscipit dolore
-      officia est illum perspiciatis unde delectus ipsam! Perspiciatis aliquid
-      exercitationem repellendus ducimus aperiam deserunt ullam illum sed, nam
-      hic culpa nobis non deleniti. Temporibus nulla optio odio minima veniam
-      rem ullam perspiciatis laboriosam, officiis maiores deserunt, possimus
-      necessitatibus hic in. Aperiam nostrum eum laudantium quis qui vitae, hic
-      voluptates vel aspernatur cupiditate! Architecto illo aut tenetur ipsum
-      nulla ea atque excepturi voluptate consequuntur.
-    </ExpandableText>
-  );
+  return <Form />;
 };
 
 export default App;
