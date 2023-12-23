7ea0e15998cc79901ac3d414e9a4881db6585612
elen-oz <ozerova.lenka@gmail.com>
2023-12-23 19:54:45 +0100

    expanded text

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import { useState } from 'react';
 import Like from './components/Like';
+import ExpandableText from './components/ExpandableText';
 
 const App = () => {
-  return <Like onClick={() => console.log('clicked')} />;
+  const handleClick = () => {};
+
+  return (
+    // <Like onClick={handleClick} />
+    <ExpandableText>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam accusantium
+      assumenda, nobis quia nemo, veniam itaque voluptates at cumque labore
+      temporibus! Quo distinctio perferendis ipsa quasi, nisi laboriosam quis
+      quidem necessitatibus esse doloremque cumque laborum suscipit dolore
+      officia est illum perspiciatis unde delectus ipsam! Perspiciatis aliquid
+      exercitationem repellendus ducimus aperiam deserunt ullam illum sed, nam
+      hic culpa nobis non deleniti. Temporibus nulla optio odio minima veniam
+      rem ullam perspiciatis laboriosam, officiis maiores deserunt, possimus
+      necessitatibus hic in. Aperiam nostrum eum laudantium quis qui vitae, hic
+      voluptates vel aspernatur cupiditate! Architecto illo aut tenetur ipsum
+      nulla ea atque excepturi voluptate consequuntur.
+    </ExpandableText>
+  );
 };
 
 export default App;
