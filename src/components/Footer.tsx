f4f8ab24a8331748ba8c358c7e666ab4a52ec57b
elen-oz <ozerova.lenka@gmail.com>
2023-12-26 00:03:47 +0100

    change footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,12 +2,21 @@ const Footer = () => {
   return (
     <footer className='footer mt-auto py-2 bg-light'>
       <div className='text-center'>
+        made by{' '}
+        <a
+          href='https://github.com/elen-oz'
+          target='_blank'
+          className='link-secondary'
+        >
+          elen-oz
+        </a>
+        &nbsp;&nbsp;
         <a
           href='https://github.com/elen-oz/hi_expenses-app'
           target='_blank'
           className='link-success'
         >
-          Source Code
+          source code
         </a>
       </div>
     </footer>
