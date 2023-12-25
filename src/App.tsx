5210987fc71584b52a8ba4089132d3fd3fa02aee
elen-oz <ozerova.lenka@gmail.com>
2023-12-25 23:46:46 +0100

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import ExpenseList from './components/ExpenseList';
 import ExpenseFilter from './components/ExpenseFilter';
 import ExpenseForm from './components/ExpenseForm';
 import listOfExpenses from './utils/expenses';
+import Footer from './components/Footer';
 
 const App = () => {
   const [selectedCategory, setSelectedCategory] = useState('');
@@ -14,29 +15,32 @@ const App = () => {
     : expenses;
 
   return (
-    <div className='container-sm custom-sm-container'>
-      <h2 className='my-3 text-center'>Calculate Your Expenses</h2>
-      <div className='mb-5'>
-        <ExpenseForm
-          onSubmit={(newExpense) =>
-            setExpenses([
-              ...expenses,
-              { ...newExpense, id: expenses.length + 1 },
-            ])
-          }
-        />
-      </div>
-      <div className='mb-3'>
-        <ExpenseFilter
-          onSelectCategory={(category) => setSelectedCategory(category)}
-        />
-      </div>
+    <>
+      <div className='container-sm custom-sm-container'>
+        <h2 className='my-3 text-center'>Calculate Your Expenses</h2>
+        <div className='mb-5'>
+          <ExpenseForm
+            onSubmit={(newExpense) =>
+              setExpenses([
+                ...expenses,
+                { ...newExpense, id: expenses.length + 1 },
+              ])
+            }
+          />
+        </div>
+        <div className='mb-3'>
+          <ExpenseFilter
+            onSelectCategory={(category) => setSelectedCategory(category)}
+          />
+        </div>
 
-      <ExpenseList
-        expenses={visibleExpenses}
-        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
-      />
-    </div>
+        <ExpenseList
+          expenses={visibleExpenses}
+          onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+        />
+      </div>
+      <Footer />
+    </>
   );
 };
 
---
+++ src/components/Footer.tsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <footer className='footer mt-auto py-2 bg-light'>
+      <div className='text-center'>
+        <a
+          href='https://github.com/elen-oz/hi_expenses-app'
+          target='_blank'
+          className='link-success'
+        >
+          Source Code
+        </a>
+      </div>
+    </footer>
+  );
+};
+export default Footer;
